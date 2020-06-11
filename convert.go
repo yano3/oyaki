762fff425287ea0e5f10696bb6b7d509e621c503
Teppei Yano <buckelwal@gmail.com>
2020-06-11 20:14:07 +0900

    stop supporting converion from png

--- convert.go
+++ convert.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"image"
-	"image/draw"
 	_ "image/gif"
 	"image/jpeg"
 	_ "image/png"
@@ -11,24 +10,12 @@ import (
 )
 
 func convert(src io.Reader, q int) (*bytes.Buffer, error) {
-	img, format, err := image.Decode(src)
+	img, _, err := image.Decode(src)
 	if err != nil {
 		return nil, err
 	}
 
 	buf := bytes.NewBuffer(nil)
-
-	if format == "png" {
-		bg := image.NewUniform(image.White)
-		dstImg := image.NewRGBA(img.Bounds())
-
-		draw.Draw(dstImg, dstImg.Bounds(), bg, image.ZP, draw.Src)
-		draw.Draw(dstImg, dstImg.Bounds(), img, image.ZP, draw.Over)
-
-		if err := jpeg.Encode(buf, dstImg, &jpeg.Options{Quality: q}); err != nil {
-			return nil, err
-		}
-	}
 
 	if err := jpeg.Encode(buf, img, &jpeg.Options{Quality: quality}); err != nil {
 		return nil, err
