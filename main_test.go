76f57fe5dca66567f1a4ad61af52828cd0e7fa16
Teppei Yano <buckelwal@gmail.com>
2021-01-20 18:50:51 +0900

    add tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,5 +14,94 @@ func TestRoot(t *testing.T) {
 
 	if rec.Code != http.StatusOK {
 		t.Errorf("got http %d, want %d", rec.Code, http.StatusOK)
+	}
+}
+
+func TestProxyJPEG(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(proxy))
+	fmt.Println(ts.URL)
+
+	origin := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./testdata/oyaki.jpg")
+	}))
+
+	orgSrvURL = origin.URL
+
+	url := ts.URL + "/oyaki.jpg"
+
+	res, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	orgRes, err := http.Get(orgSrvURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("HTTP status is %d, want %d", res.StatusCode, http.StatusOK)
+	}
+
+	if res.ContentLength < 0 {
+		t.Errorf("Content-Length header does not exist")
+	}
+
+	if res.ContentLength >= orgRes.ContentLength {
+		t.Errorf("value of Content-Length header %d is larger than origin's one %d", res.ContentLength, orgRes.ContentLength)
+	}
+}
+
+func TestProxyPNG(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(proxy))
+
+	origin := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./testdata/corn.png")
+	}))
+
+	orgSrvURL = origin.URL
+	url := ts.URL + "/corn.png"
+
+	res, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	orgRes, err := http.Get(orgSrvURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("HTTP status is %d, want %d", res.StatusCode, http.StatusOK)
+	}
+
+	if res.ContentLength < 0 {
+		t.Errorf("Content-Length header does not exist")
+	}
+
+	if res.ContentLength != orgRes.ContentLength {
+		t.Errorf("value of Content-Length header %d is not equal to origin's one, want %d", res.ContentLength, orgRes.ContentLength)
+	}
+}
+
+func TestOriginNotExist(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(proxy))
+
+	origin := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+	}))
+
+	orgSrvURL = origin.URL
+
+	url := ts.URL + "/none.jpg"
+
+	res, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusBadGateway {
+		t.Errorf("HTTP status is %d, want %d", res.StatusCode, http.StatusBadGateway)
 	}
 }
